6717ff44200bee3e770b6ab13755f55751884873
mohammad <m.roghani1212@gmail.com>
2019-12-16 16:54:39 +0330

    BlogPost model added.

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100)
+    overview = models.CharField(max_length=500)
+    text = models.CharField(max_length=5000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=False)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
