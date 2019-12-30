33860bb31d38376d1210af3b825f52a1a1e861e3
mohammad <m.roghani1212@gmail.com>
2019-12-30 17:59:40 +0330

    logout added

--- myblog/authh/views.py
+++ myblog/authh/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, HttpResponse
 
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -8,11 +8,15 @@ def login_view(request):
     if request.method == 'GET':
         return render(request, 'login.html')
     if request.method == 'POST':
+        action = request.POST.get('action')
+        if action == 'logout':
+            logout(request)
+            return HttpResponse('successfully logged out')
         user = request.POST.get('username')
         passwd = request.POST.get('password')
         user = authenticate(username=user, password=passwd)
         if user is None:
-            return HttpResponse("failed")
+            return HttpResponse("wrong username or password")
         else:
             login(request, user)
             return HttpResponse(request.user)
